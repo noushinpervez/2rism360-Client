41ef51332307957dffe572a77751c79f1c00df23
noushinpervez <134955206+noushinpervez@users.noreply.github.com>
2024-05-02 08:51:47 +0600

    added countries component

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,8 +1,8 @@
 const Countries = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="lg:py-20 py-10 bg-background">
+            <h3 className="text-2xl text-center mb-6">Countries</h3>
+        </section>
     );
 };
 
