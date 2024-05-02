2e3907a9cd5d56716b0fe018336deab3bcbbdd7a
noushinpervez <134955206+noushinpervez@users.noreply.github.com>
2024-05-02 09:01:37 +0600

    added individual country headline

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,9 @@
+const Country = () => {
+    return (
+        <section className="p-6 lg:p-12 bg-background">
+            <h3 className="text-2xl text-center mb-6">Country</h3>
+        </section>
+    );
+};
+
+export default Country;
